6fffe0412e95b50be83c85f886b38a0768227e48
ga53xit <ga53xit@mytum.de>
2022-06-30 19:23:26 +0200

    Percentile smoothing funktion wie in paper 'real-time...'

--- code/visualize_test.py
+++ code/visualize_test.py
@@ -21,6 +21,26 @@ def importData(datafile):
 
     return df
 
+def importRawSignals(datafile):
+    try:
+        new_name = datafile.replace(".xlsx", ".npy")
+        with open(new_name, 'rb') as f:
+            df = np.load(f)
+            data = df
+    except:
+        df = pd.read_excel(io=datafile)
+        df = np.asarray(df)
+        new_name = datafile.replace(".xlsx", ".npy")
+        with open(new_name, 'wb') as f:
+            np.save(f, df)
+            data = df
+
+    signal1 = data[:, 6:117]
+    signal2 = data[:, 230:341]
+
+    return signal1, signal2
+
+
 
 def testCorrelation(data):
 
@@ -30,8 +50,8 @@ def testCorrelation(data):
 
 
     randomlist = []
-    for i in range(0, 1):
-        n = i # random.randint(1, 1300)
+    for i in range(0, 20):
+        n =  random.randint(1, 1300)
         randomlist.append(n)
 
     for x in randomlist:
@@ -48,27 +68,44 @@ def testCorrelation(data):
         signal2_DN = savgol_filter(signal2, 111, 9) # window size , polynomial order
         signal1_DN_s  = savgol_filter(signal1, 111, 9)
 
+        percentile_val = 91
+        num_of_percentile_windows = 30
+        #print(signal1.size)
+        window_size = int(signal1.size/num_of_percentile_windows)
+        #print(window_size)
+
+        signal1_pct, signal2_pct = np.zeros(num_of_percentile_windows), np.zeros(num_of_percentile_windows)
+
+
+        for i in range(num_of_percentile_windows-1):
+
+            signal1_pct[i] = np.percentile(signal1[i*window_size:(i+1)*window_size], percentile_val)
+            signal2_pct[i] = np.percentile(signal2[i*window_size:(i+1)*window_size], percentile_val)
+
         ave1 = np.average(signal1_DN_s)
         ave2 = np.average(signal2_DN)
 
-        #signal1_DN_s = signal1_DN_s + (ave2 - ave1)
-        signals = np.vstack((signal1_DN_s, signal2_DN))
-        #signals = signals.T
-        print(signals.shape)
 
-        r = np.corrcoef(signal1_DN_s, signal2_DN)
-        print('cov:', r)
+        #signals = np.vstack((signal1_DN_s, signal2_DN))
+
+
+
+        #r = np.corrcoef(signal1_DN_s, signal2_DN)
+        r_pct = np.corrcoef((signal1_pct, signal2_pct))
+        print('cov:', r_pct[0, 1])
 
         plt.figure()
-        plt.title("Sample " + str(x) + " Correlation = " + str(r[0, 1]))
+        plt.title("Sample " + str(x) + " Correlation = " + str(r_pct[0, 1]))
 
-        plt.plot(signal1, label="Signal 1")
-        plt.plot(signal2, label="Signal 2")
-        plt.plot(signal1_DN, label="Signal 1 DN original")
-        plt.plot(signal2_DN, label="Signal 2 DN")
-        plt.plot(signal1_DN_s, label="Signal 1 DN self made")
+       # plt.plot(signal1, label="Signal 1")
+        #plt.plot(signal2, label="Signal 2")
+        #plt.plot(signal1_DN, label="Signal 1 DN original")
+        #plt.plot(signal2_DN, label="Signal 2 DN")
+        #plt.plot(signal1_DN_s, label="Signal 1 DN self made")
+        plt.plot(signal1_pct, label="Signal 1 percentile")
+        plt.plot(signal2_pct, label="Signal 2 percentile")
         plt.legend()
-        #plt.show()
+        plt.show()
         #plt.savefig("../DataCorrelation/" + str(x) + ".png", dpi=500)
         plt.close()
 ##
