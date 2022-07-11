53fa712e881dc74dfb22da1a9f403392af93c93c
tsvet499 <tsvetkovnikita@yandex.com>
2022-07-11 16:26:09 +0200

    Logistic reg file commit

--- code/Logistic_Regression_Nikita.py
+++ code/Logistic_Regression_Nikita.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     #TH1_DN = "../OOT/S1_DN_OOT_ONLY.xlsx"
     #TH2 = "../OOT/S2_OOT_ONLY.xlsx"
 
-    data, data_just_signal = read_pandas(S1)
+    data, data_just_signal = read_pandas(S1_DN)
     mean, std = mean_and_std(data_just_signal)
 
     rmsvalues = pd.Series([RMS(e[1]) for e in data_just_signal.iterrows()])
@@ -40,15 +40,18 @@ if __name__ == "__main__":
 
     #X = data_just_signal                                                            #RAW DATA as feature -> Accurace = 0.910
     #X = mean.to_numpy().reshape(-1, 1)                                              #mean as feature -> Accurace = 0.81
-    #X = std.to_numpy().reshape(-1, 1)                                               #std as feature -> Accuracy = 0.785
+    X = std.to_numpy().reshape(-1, 1)                                               #std as feature -> Accuracy = 0.785
     #X = rmsvalues.to_numpy().reshape(-1, 1)                                         #rms as feature -> Accuracy = 0.822
     #X = np.stack((std.to_numpy() ,mean.to_numpy()), axis = 1).reshape(-1, 2)        #mean and std as feature ->  Accuracy = 0.822
 
     #y = data["not OK"].to_numpy()
-    y = data["WD40"].to_numpy()                                                        # ->  Accuracy = 0.668 with RAW
+    #y = data["WD40"].to_numpy()                                                        # ->  Accuracy = 0.668 with RAW
     #y  = data["Gleitmo"].to_numpy()                                                   # ->  Accuracy = 0.696 with RAW
+    y = data["WD40"].to_numpy() + data["Gleitmo"].to_numpy()
+    y =  np.minimum(y, 1)                                                               #   Accuracy = 0.67
+    print(y)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state = 43)  # ;)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30)  # ;)
     #print(X_train, X_test)
 
     ### Pipeline Solution ###
