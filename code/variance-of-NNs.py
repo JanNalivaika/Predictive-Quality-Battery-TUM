691be474864dbbca3abd9e2d3bb0795585ff7e0d
ga53xit <ga53xit@mytum.de>
2022-07-12 23:21:07 +0200

    k-fold

--- code/variance-of-NNs.py
+++ code/variance-of-NNs.py
@@ -1,5 +1,9 @@
 import numpy as np
+import pandas as pd
 from NN_v2 import main
+from NN_Kvalidation import main as mainKval
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
 
 """
@@ -10,7 +14,19 @@ step_neurons: schrittweite der betrachteten kotenzahl
 Für alle kombinationen wird der Mittelwert der Test Accuracy und die Varianz gespeichert
 """
 
-num_hidden_layers = 5
+def importSignal(datafile):
+    df = pd.read_excel(io=datafile)
+    ok_label = df.loc[:, 'not OK']
+    ok_label = np.asarray(ok_label)
+
+    df_just_signal = df.drop(['not OK', 'WD40', 'Gleitmo'], axis=1)
+    signal1 = np.asarray(df_just_signal)
+
+    return signal1, ok_label
+
+
+
+num_hidden_layers = 6
 num_neurons = 100
 step_neurons = 5
 num_iterations = 50
@@ -23,16 +39,35 @@ x2 = neurons.size
 x3 = 2
 mean_err = np.zeros((x1, x2, x3))
 
-temp_acc = np.zeros((num_iterations))
+#import data
+datafile = "../Data/S1_DN_relabeled.xlsx"
+signal, nok = importSignal(datafile)
 
+X = signal
+y = nok
+
+#split data
+#num_splits = int(np.sqrt(len(y)))
+num_splits = 10
+skf = StratifiedKFold(n_splits=num_splits, shuffle=True, random_state=42)
+
+
+temp_acc = np.zeros((num_splits))
 for l in range(0, hid_layers.size):
 
 
     for n in range(0, neurons.size):
         print('l:', hid_layers[l], ' n:', neurons[n])
-        for i in range(num_iterations):
-            temp_acc[i] = main(hid_layers[l], neurons[n])
-            print(temp_acc[i])
+
+        split_iteration = 0
+        for train_index, test_index in skf.split(X, y):
+            #print("TRAIN:", train_index, "TEST:", test_index)
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+
+            temp_acc[split_iteration] = mainKval(hid_layers[l], neurons[n], X_train, y_train, X_test, y_test)
+            print(temp_acc[split_iteration])
+            split_iteration += 1
 
         mean_err[l, n, 0] = np.mean(temp_acc)
         mean_err[l, n, 1] = np.max(temp_acc) - np.mean(temp_acc)
@@ -51,7 +86,7 @@ for h in range(0, hid_layers.size):
     plt.errorbar(neurons, mean_err[h, :, 0], mean_err[h, :, 1], capsize=10,
              capthick=1, ls='none')
     plt.show()
-    fig.savefig('No. of hidden layers' + str(hid_layers[h]) + '.jpg', bbox_inches='tight', dpi=250)
+    #fig.savefig('No. of hidden layers' + str(hid_layers[h]) + '.jpg', bbox_inches='tight', dpi=250)
 
 
 
